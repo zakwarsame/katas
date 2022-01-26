c32d8ae68e274ba098a85282d04d66fdc5e0b573
alchemy <zakwarsame2018@gmail.com>
2022-01-26 15:37:54 -0500

    add details

--- findOdd.js
+++ findOdd.js
@@ -1,5 +1,8 @@
+/**
+Given an array of integers, find the one that appears an odd number of times.
 
-
+There will always be only one integer that appears an odd number of times.
+**/
 
 function isOdd(num) {
     return num % 2 !== 0;
@@ -8,28 +11,21 @@ function isOdd(num) {
   function findOdd(arrOfIntegers) {
     //happy coding!
   
-    let numTrackingObj = {
-    };
-
-
-    // arrOfIntegers
-    // .forEach(function(el){
-    //     numTrackingObj[el] ? numTrackingObj[el]++ : numTrackingObj[el] = 1;
-    //   });
+    let numTrackingObj = {};
   
     for (let i = 0; i < arrOfIntegers.length; i++) {
-      if (numTrackingObj[arrOfIntegers[i]] >= 0 ) {
+      if (numTrackingObj[arrOfIntegers[i]] >= 0) {
         numTrackingObj[arrOfIntegers[i]]++;
       } else {
         numTrackingObj[arrOfIntegers[i]] = 1;
       }
     }
   
-    for (const val in numTrackingObj){
-        if(isOdd(numTrackingObj[val])) return val
+    for (const val in numTrackingObj) {
+      if (isOdd(numTrackingObj[val])) return val;
     }
-    return 0
+    return 0;
   }
   
-  console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+  console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
   

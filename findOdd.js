68d875b2136a86795179ecc8287d062cb703e65d
alchemy <zakwarsame2018@gmail.com>
2022-01-26 15:29:29 -0500

    add question deets

---
+++ findOdd.js
@@ -0,0 +1,35 @@
+
+
+
+function isOdd(num) {
+    return num % 2 !== 0;
+  }
+  
+  function findOdd(arrOfIntegers) {
+    //happy coding!
+  
+    let numTrackingObj = {
+    };
+
+
+    // arrOfIntegers
+    // .forEach(function(el){
+    //     numTrackingObj[el] ? numTrackingObj[el]++ : numTrackingObj[el] = 1;
+    //   });
+  
+    for (let i = 0; i < arrOfIntegers.length; i++) {
+      if (numTrackingObj[arrOfIntegers[i]] >= 0 ) {
+        numTrackingObj[arrOfIntegers[i]]++;
+      } else {
+        numTrackingObj[arrOfIntegers[i]] = 1;
+      }
+    }
+  
+    for (const val in numTrackingObj){
+        if(isOdd(numTrackingObj[val])) return val
+    }
+    return 0
+  }
+  
+  console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+  

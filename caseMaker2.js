608c291504a4426aaabae110bc7a34d32fc496a6
zak <zakwarsamee@gmail.com>
2022-04-10 17:23:50 -0400

    case maker 2

--- caseMaker2.js
+++ caseMaker2.js
@@ -77,11 +77,11 @@ const makeCase = function (input, word) {
   return input;
 };
 
-// console.log(makeCase("this is a string", "camel"));
-// console.log(makeCase("this is a string", "pascal"));
-// console.log(makeCase("this is a string", "snake"));
-// console.log(makeCase("this is a string", "kebab"));
-// console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "camel"));
+console.log(makeCase("this is a string", "pascal"));
+console.log(makeCase("this is a string", "snake"));
+console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
-// console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", "consonant"));
 console.log(makeCase("this is a string", ["upper", "snake"]));

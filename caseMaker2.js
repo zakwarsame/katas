ab27eda9b9aafb6c65617b78be9a5eef8e3860b8
zak <zakwarsamee@gmail.com>
2022-04-10 17:23:35 -0400

    case maker 2

--- caseMaker2.js
+++ caseMaker2.js
@@ -1,44 +1,87 @@
-const makeCase = function(input, word) {
-    // Put your solution here
-    let wordArray = [];
-    let convertedString = "";
-    let rest = "";
+/**
+ * Create a function named makeCase that will receive an input string and one or more casing options. Return a new string that is formatted based on casing options:
+ * @param {*} input
+ * @param {*} word
+ * @returns
+ */
 
-    let firstWord = input.split(" ").slice(0,1).join("");
-    let restArray = input.split(" ").slice(1)
+const makeCase = function (input, word) {
+  // Put your solution here
+  let wordArray = [];
 
-    if(Array.isArray(word)) {
-        wordArray = [...word]
-    } else {
-        wordArray.push(word)
-    }
-
-
-
-
-
-   let something =  wordArray.map((caseStyle)=> {
-        switch (caseStyle) {
-            case "camel":
-                rest = restArray.map(e=> e.charAt(0).toUpperCase() + e.slice(1)).join("")
-                return firstWord + rest
-
-            case "pascal":
-                firstWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1)
-                rest = restArray.map(e=> e.charAt(0).toUpperCase() + e.slice(1)).join("")
-                return firstWord + rest
-        }
-    })
-
-    return 
+  if (Array.isArray(word)) {
+    wordArray = [...word];
+  } else {
+    wordArray.push(word);
   }
 
+  if (wordArray.length > 1) {
+    const sortingArr = [
+      "camel",
+      "pascal",
+      "snake",
+      "kebab",
+      "title",
+      "vowel",
+      "consonant",
+      "upper",
+      "lower",
+    ];
+    wordArray.sort((a, b) => sortingArr.indexOf(a) - sortingArr.indexOf(b));
+  }
 
-console.log(makeCase("this is a string", "camel"));
-console.log(makeCase("this is a string", "pascal"));
-console.log(makeCase("this is a string", "snake"));
-console.log(makeCase("this is a string", "kebab"));
-console.log(makeCase("this is a string", "title"));
+  wordArray.map((caseStyle) => {
+    switch (caseStyle) {
+      case "camel":
+        input = input
+          .replace(/\s\w/g, (char) => char.toUpperCase())
+          .replace(/\s/g, "");
+        break;
+
+      case "pascal":
+        input = input
+          .replace(/\b\w/g, (char) => char.toUpperCase())
+          .replace(/\s/g, "");
+        break;
+
+      case "snake":
+        input = input.replace(/\s/g, "_");
+        break;
+
+      case "kebab":
+        input = input.replace(/\s/g, "-");
+        break;
+
+      case "title":
+        input = input.replace(/\b\w/g, (char) => char.toUpperCase());
+        break;
+
+      case "vowel":
+        input = input.replace(/[aeiou]/gi, (char) => char.toUpperCase());
+        break;
+
+      case "consonant":
+        input = input.replace(/[^aeiou]/gi, (char) => char.toUpperCase());
+        break;
+
+      case "upper":
+        input = input.toUpperCase();
+        break;
+
+      case "lower":
+        input = input.toLowerCase();
+        break;
+    }
+  });
+
+  return input;
+};
+
+// console.log(makeCase("this is a string", "camel"));
+// console.log(makeCase("this is a string", "pascal"));
+// console.log(makeCase("this is a string", "snake"));
+// console.log(makeCase("this is a string", "kebab"));
+// console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
-console.log(makeCase("this is a string", "consonant"));
-console.log(makeCase("this is a string", ["camel", "pascal"]));
+// console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
